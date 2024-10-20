f6e3cb0c93a77ca7ff6700eb77fea22a383af386
jlgsobreiro <jlgsobreiro@gmail.com>
2024-10-20 08:40:38 -0300

    Refactor database_models

--- databases/databases_model.py
+++ databases/databases_model.py
@@ -1,32 +1,31 @@
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, connect, IntField
 
 from databases.mongo_model import MongodbClient
 from databases.mysql_model import MySQLConnection
 from databases.redis_model import RedisClient
 
 
-# from databases.redis_model import RedisClient
-
-
 class Databases(Document):
-    name = StringField(required=True)
+    connect('databases')
+    table = StringField(required=True)
     host = StringField(required=True)
-    port = StringField(required=True)
+    port = IntField(required=True)
     database = StringField(required=True)
+    user = StringField()
+    password = StringField()
 
-    def instantiate(self):
+    def get_connection(self):
         if self.database == 'mysql':
             return MySQLConnection(
                 host=self.host,
                 port=self.port,
-                target = self.name,
-                user='root',
-                password='rootpassword'
+                target = self.table,
+                user=self.user,
+                password=self.password
             )
         elif self.database == 'redis':
-            print(f'Connecting to Redis at {self.host}:{self.port}:{self.name}')
-            return RedisClient(host=self.host, port=self.port, collection=self.name)
+            return RedisClient(host=self.host, port=self.port, collection=self.table)
         elif self.database == 'mongodb':
-            return MongodbClient(host=self.host, port=self.port, target=self.name)
+            return MongodbClient(host=self.host, port=self.port, collection=self.table)
         else:
             raise ValueError('Database not supported')
