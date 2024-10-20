65610650c830b24a72fa2e6f6d110b0b6c46fb43
jlgsobreiro <jlgsobreiro@gmail.com>
2024-10-20 08:41:10 -0300

    Refactor

--- databases/mongo_model.py
+++ databases/mongo_model.py
@@ -2,13 +2,13 @@ from pymongo import MongoClient
 
 
 class MongodbClient:
-    def __init__(self, host='localhost', port=27017, target=None):
+    def __init__(self, host='localhost', port=27017, collection=None):
         self.client = MongoClient(host, port)
         self.db = self.client['test']
-        self.collection = self.db[target]
+        self.collection = self.db[collection]
 
 if __name__ == '__main__':
-    client = MongodbClient(target='test')
+    client = MongodbClient(collection='test')
     client.collection.insert_one({'key': 'value'})
     assert (client.collection.find_one({'key': 'value'}).get('key')) == 'value'
     client.client.close()
